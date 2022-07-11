7b801bb04e4b8a937544bfa3854bf06b7737985f
Crazy H <87752773+CrazyH2@users.noreply.github.com>
2022-07-11 11:55:26 +0100

    Update addBootbox.js
    IMPORTING ALL NEEDED FILES FOR USING BOOTBOXJS

--- addBootbox.js
+++ addBootbox.js
@@ -9,15 +9,19 @@
 
  //  Importing Bootbox.js Files
 
- //  
-import * from 'utils.js';
- //  
-import * from 'utils.js';
- //  
-import * from 'utils.js';
- //  
-import * from 'utils.js';
- //  
-import * from 'utils.js';
- //  
-import * from 'utils.js';
+ //  BOOTSTRAP.MIN.CSS
+import * from 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0-beta1/css/bootstrap.min.css';
+ //  JQUERY.MIN.JS
+import * from 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+ //  BOOTSTRAP.MIN.JS
+import * from 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0-beta1/js/bootstrap.min.js';
+ //  BOOTBOX.MIN.JS
+import * from 'https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.3/bootbox.min.js';
+
+
+ /////////////////////////////////////////////////
+ //  © Copyright 2022 CrazyH                    //
+ //  This file was originally made by CrazyH    //
+ //  Do not rebrand if you are distributing it  //
+ //  © Copyright 2022 CrazyH                    //
+ /////////////////////////////////////////////////
