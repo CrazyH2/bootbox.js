dacea9554c78f996e67bf1a7bff533f2d5a613c0
Crazy H <87752773+CrazyH2@users.noreply.github.com>
2022-07-11 13:23:22 +0100

    Update addBootbox.js

--- addBootbox.js
+++ addBootbox.js
@@ -11,25 +11,37 @@
 
 console.clear()
  //  BOOTSTRAP.MIN.CSS
-import * from 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0-beta1/css/bootstrap.min.css';
+import 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0-beta1/css/bootstrap.min.css';
  //  JQUERY.MIN.JS
-import * from 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+import 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js';
  //  BOOTSTRAP.MIN.JS
-import * from 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0-beta1/js/bootstrap.min.js';
+import 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0-beta1/js/bootstrap.min.js';
  //  BOOTBOX.MIN.JS
-import * from 'https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.3/bootbox.min.js';
+import 'https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.3/bootbox.min.js';
 
 
  //  Importing Bootbox.js Files
 var msg = `%c
-  ____              _   _                     _ ____  
- | __ )  ___   ___ | |_| |__   _____  __     | / ___| 
- |  _ \ / _ \ / _ \| __| '_ \ / _ \ \/ /  _  | \___ \ 
- | |_) | (_) | (_) | |_| |_) | (_) >  <  | |_| |___) |
- |____/ \___/ \___/ \__|_.__/ \___/_/\_\  \___/|____/ 
-                                                      
+██████   ██████   ██████  ████████ ██████   ██████  ██   ██          ██ ███████ 
+██   ██ ██    ██ ██    ██    ██    ██   ██ ██    ██  ██ ██           ██ ██      
+██████  ██    ██ ██    ██    ██    ██████  ██    ██   ███            ██ ███████ 
+██   ██ ██    ██ ██    ██    ██    ██   ██ ██    ██  ██ ██      ██   ██      ██ 
+██████   ██████   ██████     ██    ██████   ██████  ██   ██      █████  ███████ 
+                                                                                
+                                                                                
+LOADED ALL IMPORTS AND FILES FROM BOOTBOXJS ...
 `
-console.info(msg, 'color: #D81A6D; font-weight: bold; font-family: monospace;');
+console.info(msg, 'color: #D81A6D; font-weight: bold; font-family: monospace; text-align: center;');
+var msg = `%c
+ /////////////////////////////////////////////////
+ //  © Copyright 2022 CrazyH                    //
+ //  This file was originally made by CrazyH    //
+ //  Do not rebrand if you are distributing it  //
+ //  © Copyright 2022 CrazyH                    //
+ /////////////////////////////////////////////////
+
+`
+console.info(msg, 'color: #D81A6D; font-weight: bold; font-family: monospace; text-align: center;');
 
 
  /////////////////////////////////////////////////
