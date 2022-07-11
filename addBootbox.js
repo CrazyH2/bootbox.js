42c21a6d06c8f04fcfa563969921ed4f28d4fd36
Crazy H <87752773+CrazyH2@users.noreply.github.com>
2022-07-11 12:58:50 +0100

    Update addBootbox.js

--- addBootbox.js
+++ addBootbox.js
@@ -9,6 +9,7 @@
 
  //  Importing Bootbox.js Files
 
+console.clear()
  //  BOOTSTRAP.MIN.CSS
 import * from 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0-beta1/css/bootstrap.min.css';
  //  JQUERY.MIN.JS
@@ -17,6 +18,18 @@ import * from 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js
 import * from 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0-beta1/js/bootstrap.min.js';
  //  BOOTBOX.MIN.JS
 import * from 'https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.3/bootbox.min.js';
+
+
+ //  Importing Bootbox.js Files
+var msg = `%c
+  ____              _   _                     _ ____  
+ | __ )  ___   ___ | |_| |__   _____  __     | / ___| 
+ |  _ \ / _ \ / _ \| __| '_ \ / _ \ \/ /  _  | \___ \ 
+ | |_) | (_) | (_) | |_| |_) | (_) >  <  | |_| |___) |
+ |____/ \___/ \___/ \__|_.__/ \___/_/\_\  \___/|____/ 
+                                                      
+`
+console.info(msg, 'color: #D81A6D; font-weight: bold; font-family: monospace;');
 
 
  /////////////////////////////////////////////////
