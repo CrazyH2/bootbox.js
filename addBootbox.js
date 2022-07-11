627a38665ef02ecba20675bdc44a24aadd491857
Crazy H <87752773+CrazyH2@users.noreply.github.com>
2022-07-11 13:27:50 +0100

    Update addBootbox.js

--- addBootbox.js
+++ addBootbox.js
@@ -1,10 +1,24 @@
+ ///////////////////////////////////////////////////////////////////////////////////////
+ //                                                                                   //
+ //  ██████   ██████   ██████  ████████ ██████   ██████  ██   ██          ██ ███████  //
+ //  ██   ██ ██    ██ ██    ██    ██    ██   ██ ██    ██  ██ ██           ██ ██       //
+ //  ██████  ██    ██ ██    ██    ██    ██████  ██    ██   ███            ██ ███████  //
+ //  ██   ██ ██    ██ ██    ██    ██    ██   ██ ██    ██  ██ ██      ██   ██      ██  //
+ //  ██████   ██████   ██████     ██    ██████   ██████  ██   ██      █████  ███████  //
+ //                                                                                   //
+ ///////////////////////////////////////////////////////////////////////////////////////
 
  /////////////////////////////////////////////////
+ //                                             //
  //  © Copyright 2022 CrazyH                    //
  //  This file was originally made by CrazyH    //
  //  Do not rebrand if you are distributing it  //
  //  © Copyright 2022 CrazyH                    //
+ //                                             //
  /////////////////////////////////////////////////
+
+
+
 
 
  //  Importing Bootbox.js Files
@@ -34,19 +48,13 @@ LOADED ALL IMPORTS AND FILES FROM BOOTBOXJS ...
 console.info(msg, 'color: #D81A6D; font-weight: bold; font-family: monospace; text-align: center;');
 var msg = `%c
  /////////////////////////////////////////////////
+ //                                             //
  //  © Copyright 2022 CrazyH                    //
  //  This file was originally made by CrazyH    //
  //  Do not rebrand if you are distributing it  //
  //  © Copyright 2022 CrazyH                    //
+ //                                             //
  /////////////////////////////////////////////////
 
 `
 console.info(msg, 'color: #D81A6D; font-weight: bold; font-family: monospace; text-align: center;');
-
-
- /////////////////////////////////////////////////
- //  © Copyright 2022 CrazyH                    //
- //  This file was originally made by CrazyH    //
- //  Do not rebrand if you are distributing it  //
- //  © Copyright 2022 CrazyH                    //
- /////////////////////////////////////////////////
